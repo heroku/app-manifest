abde79cef9860a0359d9d74ec7bb115be05e13b1
Josh W Lewis <josh.w.lewis@gmail.com>
2017-07-12 12:43:15 -0500

    Test manifest attributes

--- test/app_manifest/manifest_test.rb
+++ test/app_manifest/manifest_test.rb
@@ -1,28 +1,43 @@
 require 'test_helper'
 
-class AppManifest::ManifestTest < MiniTest::Test
-  def test_initialize
-    manifest = AppManifest::Manifest.new(name: 'my-app')
-    assert_kind_of(AppManifest::Manifest, manifest)
-  end
+module AppManifest
+  class ManifestTest < MiniTest::Test
+    def test_initialize
+      manifest = AppManifest::Manifest.new(name: 'my-app')
+      assert_kind_of(AppManifest::Manifest, manifest)
+    end
 
-  def test_initialize__canonicalizes_hash
-    manifest = AppManifest::Manifest.new(env: { 'BAR' => 'BAZ' })
-    env = manifest.to_hash.fetch(:env)
-    assert_equal(env, 'BAR' => { value: 'BAZ' })
-  end
+    def test_initialize__canonicalizes_hash
+      manifest = AppManifest::Manifest.new(env: { 'BAR' => 'BAZ' })
+      env = manifest.to_hash.fetch(:env)
+      assert_equal(env, 'BAR' => { value: 'BAZ' })
+    end
 
-  def test_environment__when_present
-    hash = { environments: { test: { addons: [{ plan: 'a' }] } } }
-    manifest = AppManifest::Manifest.new(hash)
-    test_manifest = manifest.environment(:test)
-    assert_equal(test_manifest.to_hash.fetch(:addons), [plan: 'a'])
-  end
+    def test_environment__when_present
+      hash = { environments: { test: { addons: [{ plan: 'a' }] } } }
+      manifest = AppManifest::Manifest.new(hash)
+      test_manifest = manifest.environment(:test)
+      assert_equal(test_manifest.to_hash.fetch(:addons), [plan: 'a'])
+    end
 
-  def test_environment__when_missing
-    hash = { addons: [{ plan: 'a' }] }
-    manifest = AppManifest::Manifest.new(hash)
-    foo_manifest = manifest.environment(:foo)
-    assert_equal(foo_manifest.to_hash.fetch(:addons), [plan: 'a'])
+    def test_environment__when_missing
+      hash = { addons: [{ plan: 'a' }] }
+      manifest = AppManifest::Manifest.new(hash)
+      foo_manifest = manifest.environment(:foo)
+      assert_equal(foo_manifest.to_hash.fetch(:addons), [plan: 'a'])
+    end
+
+    def test_flat_attributes
+      manifest = Manifest.new(
+        name: 'my-awesome-app',
+        description: 'super duper awesome',
+        keywords: ['awesome', 9000]
+      )
+
+      assert_equal(manifest.name, 'my-awesome-app')
+      assert_equal(manifest.description, 'super duper awesome')
+      assert_equal(manifest.keywords, %w(awesome 9000))
+      assert_nil(manifest.website)
+    end
   end
 end
