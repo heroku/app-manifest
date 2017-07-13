1cc86a9d17e16676ec2a03140913cf68b0ffafc3
Andrew Appleton <andysapple@gmail.com>
2017-07-13 10:45:47 +0100

    Fix manifest tests

--- test/app_manifest/manifest_test.rb
+++ test/app_manifest/manifest_test.rb
@@ -14,21 +14,23 @@ module AppManifest
     def test_initialize__canonicalizes_hash
       manifest = AppManifest::Manifest.new(env: { 'BAR' => 'BAZ' })
       env = manifest.to_hash.fetch(:env)
-      assert_equal(env, 'BAR' => { value: 'BAZ' })
+      assert_kind_of(Env, env['BAR'])
+      assert_equal('BAZ', env['BAR'].value)
     end
 
     def test_environment__when_present
       hash = { environments: { test: { addons: [{ plan: 'a' }] } } }
       manifest = AppManifest::Manifest.new(hash)
-      test_manifest = manifest.environment(:test)
-      assert_equal(test_manifest.to_hash.fetch(:addons), [plan: 'a'])
+      test_addons = manifest.environment(:test).addons
+      assert_kind_of(Addon, test_addons.first)
+      assert_equal('a', test_addons.first.plan)
     end
 
     def test_environment__when_missing
       hash = { addons: [{ plan: 'a' }] }
       manifest = AppManifest::Manifest.new(hash)
-      foo_manifest = manifest.environment(:foo)
-      assert_equal(foo_manifest.to_hash.fetch(:addons), [plan: 'a'])
+      foo_addons = manifest.environment(:foo).addons
+      assert_equal('a', foo_addons.first.plan)
     end
 
     def test_environments
