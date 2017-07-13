1254e772f6d225472819f7de26e399c5518a630c
Josh W Lewis <josh.w.lewis@gmail.com>
2017-07-13 17:06:17 -0500

    Change attributes to alpha order

--- lib/app_manifest/environment.rb
+++ lib/app_manifest/environment.rb
@@ -3,20 +3,19 @@ module AppManifest
     include Virtus.model
     include Serializer
 
-    attribute :name,        String
+    attribute :addons,      Array[Addon]
+    attribute :buildpacks,  Array[Buildpack]
     attribute :description, String
+    attribute :env,         Hash[String => Env]
+    attribute :formation,   Hash[String => Formation]
     attribute :image,       String
     attribute :keywords,    Array[String]
     attribute :logo,        String
+    attribute :name,        String
     attribute :repository,  String
+    attribute :scripts,     Hash[String => String]
     attribute :stack,       String
     attribute :success_url, String
     attribute :website,     String
-
-    attribute :addons,     Array[Addon]
-    attribute :buildpacks, Array[Buildpack]
-    attribute :env,        Hash[String => Env]
-    attribute :formation,  Hash[String => Formation]
-    attribute :scripts,    Hash[String => String]
   end
 end
